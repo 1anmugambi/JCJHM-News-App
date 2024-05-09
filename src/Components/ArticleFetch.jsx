8a8d6830cdb115cfce460a4a6794512ed92d90fd
jeangathoni8 <jeangathoni39@gmail.com>
2024-05-09 11:52:57 +0300

    complete project

--- src/Components/ArticleFetch.jsx
+++ src/Components/ArticleFetch.jsx
@@ -1,17 +1,20 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
+import PropTypes from 'prop-types'; // Import PropTypes
 import NewsList from '../Components/NewsList';
 import LoadingSpinner from '../Components/LoadingSpinner';
 import Error from '../Components/Error';
 
-const Home = () => {
+const Category = ({ category }) => {
   const [news, setNews] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
   useEffect(() => {
     const fetchNews = async () => {
+      let url = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=50576079b9d840e1a48ff3014f32c5bd`;
+
       try {
-        const response = await fetch('https://newsapi.org/v2/top-headlines?country=us&apiKey=50576079b9d840e1a48ff3014f32c5bd');
+        const response = await fetch(url);
         if (!response.ok) {
           throw new Error('Failed to fetch data');
         }
@@ -25,14 +28,18 @@ const Home = () => {
     };
 
     fetchNews();
-  }, []);
+  }, [category]);
 
   return (
     <div>
-      <h2>Trending News</h2>
+      <h2>Latest News in: {category}</h2>
       {loading ? <LoadingSpinner /> : error ? <Error message={error} /> : <NewsList news={news} />}
     </div>
   );
 };
 
-export default Home;
+Category.propTypes = {
+  category: PropTypes.string.isRequired // Define props validation for category
+};
+
+export default Category;
