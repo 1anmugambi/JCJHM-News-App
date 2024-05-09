78074e3fb92567edeaf15a16c666d0e8842e238a
Collins Gichuki <gichukimuchiri80@gmail.com>
2024-05-09 10:08:10 +0300

    NavBar-NewsList-Footer

---
+++ src/Components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+const Footer = () => {
+  return (
+    <div className='container-fluid d-flex justify-content-center align-items-center bg-dark text-white p-4'>
+        <h4>
+            Developed By Group [V] Members
+        </h4>
+    </div>
+  )
+}
+
+export default Footer
